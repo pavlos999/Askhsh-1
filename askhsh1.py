39ce84699acbd377169e60e9a1a4c47fe285dfc0
pavlos999 <47719637+pavlos999@users.noreply.github.com>
2019-02-17 22:08:01 +0200

    Delete askhsh1.py

--- askhsh1.py
+++
@@ -1,6 +0,0 @@
-def sum_of_intervals(intervals):
-    numbers = []
-    for interval in intervals:
-        for i in range(interval[0],interval[1]):
-            numbers.append(i)
-    return len(set(numbers))
